72ff0aa5307d49ef7f60a645f2978ab6edd8cc37
Pedro Antonio <pedroaia02@gmail.com>
2024-01-24 15:10:59 -0300

    ADD swagger sem config

--- e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
+++ e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
@@ -5,7 +5,14 @@ import com.compassuol.sp.challenge.ecommerce.domain.produto.entity.Produto;
 import com.compassuol.sp.challenge.ecommerce.domain.produto.repository.ProdutoRepository;
 import com.compassuol.sp.challenge.ecommerce.domain.produto.service.ProdutoService;
 import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoCreateDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoResponseDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.exception.ErrorMessage;
 import com.compassuol.sp.challenge.ecommerce.web.dto.mapper.ProdutoMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +30,18 @@ public class ProdutoController {
 
     private final ProdutoService produtoService;
 
+
+    @Operation(summary = "Criar um novo produto",
+            description = "Recurso para criar um novo produto no sistema. ",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ProdutoResponseDto.class))),
+                    @ApiResponse(responseCode = "500", description = "Campo invalido! Descrição deve conter 10 caracteres ou mais e o nome do produto não pode ser nulo ou vazio",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "400", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+            })
 
 
     @PostMapping("/criar")
