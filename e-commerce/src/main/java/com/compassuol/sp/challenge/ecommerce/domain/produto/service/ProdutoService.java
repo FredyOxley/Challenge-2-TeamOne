8cf064f741f5ee9794addb8dbd8a5e758be5020a
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2024-01-24 01:50:16 -0300

    Adicionando recurso para buscar produto pelo id

--- e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/domain/produto/service/ProdutoService.java
+++ e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/domain/produto/service/ProdutoService.java
@@ -1,24 +1,20 @@
 package com.compassuol.sp.challenge.ecommerce.domain.produto.service;
 
 import com.compassuol.sp.challenge.ecommerce.domain.produto.entity.Produto;
+import com.compassuol.sp.challenge.ecommerce.domain.produto.exception.EntityNotFoundException;
 import com.compassuol.sp.challenge.ecommerce.domain.produto.repository.ProdutoRepository;
 import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoCreateDto;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
-public class ProdutoService{
+public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
     private final ModelMapper modelMapper;
-
-
 
 
     public Produto salvar(ProdutoCreateDto produtoCreateDTO) {
@@ -26,6 +22,12 @@ public class ProdutoService{
         return produtoRepository.save(produto);
     }
 
+    @Transactional(readOnly = true)
+    public Produto buscarPorId(Long id) {
+        return produtoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Produto com id '%s' não encontrado no sistema.", id))
+        );
 
+    }
 }
 
--- e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
+++ e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
@@ -2,16 +2,12 @@ package com.compassuol.sp.challenge.ecommerce.web.controller;
 
 
 import com.compassuol.sp.challenge.ecommerce.domain.produto.entity.Produto;
-import com.compassuol.sp.challenge.ecommerce.domain.produto.repository.ProdutoRepository;
 import com.compassuol.sp.challenge.ecommerce.domain.produto.service.ProdutoService;
 import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoCreateDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoResponseDto;
 import com.compassuol.sp.challenge.ecommerce.web.dto.mapper.ProdutoMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +27,11 @@ public class ProdutoController {
         return ResponseEntity.ok(produtoCriado);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDto> getById(@PathVariable Long id) {
+        Produto produto = produtoService.buscarPorId(id);
+        return ResponseEntity.ok(ProdutoMapper.toDto(produto));
+    }
 
 
 
