2260e9ed0a63bc0d040de2660ee0f483a4ccfa0e
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2024-01-25 01:32:47 -0300

    Adicionando tratamento de exceção para id não encontrado no sistema.

---
+++ e-commerce/src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/exception/ApiExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.compassuol.sp.challenge.ecommerce.web.dto.exception;
+
+import com.compassuol.sp.challenge.ecommerce.domain.produto.exception.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+}
